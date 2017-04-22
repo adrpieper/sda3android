d80187712dff52ff5e23edbe07e3796242cd4218
rafkapsa <rafkapsa@gmail.com>
2017-04-22 09:30:29 +0200

    ss

--- MyApplication/app/src/main/java/com/example/adrian/world/hello/helloworld/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/adrian/world/hello/helloworld/MainActivity.java
@@ -7,6 +7,7 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //komentarz
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
