7cceca842f672b0c413b4a1b3bb89ff7667cff21
p.nowacki <p.nowacki46@gmail.com>
2017-04-22 10:22:18 +0200

    added my name and last name v2

--- MyApplication/app/src/main/java/com/example/adrian/world/hello/helloworld/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/adrian/world/hello/helloworld/MainActivity.java
@@ -7,14 +7,12 @@ import android.widget.TextView;
 public class MainActivity extends Activity {
 
 
-    private TextView myDisply;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        myDisply = (TextView) findViewById(R.id.textView_pn);
-        myDisply.setText("Przemyslaw Nowacki");
     }
 }
