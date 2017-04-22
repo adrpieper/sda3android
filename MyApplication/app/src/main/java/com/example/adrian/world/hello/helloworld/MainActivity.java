e02220d495d0b2db45fe77e40f8f2e4c52108593
Angelika Wittbrodt <angwittb@gmail.com>
2017-04-22 10:01:32 +0200

    First commit

--- MyApplication/app/src/main/java/com/example/adrian/world/hello/helloworld/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/adrian/world/hello/helloworld/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends Activity {
 
+    //First commit
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
