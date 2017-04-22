cb631ee6cbb26a90ac4b2f87c37973c61822449b
Szymon Mikitiuk <sz.mktk@gmail.com>
2017-04-22 11:15:09 +0200

    Added new project, TooBasicLoginApp

---
+++ TooBasicLoginApp/app/src/main/java/app/login/toobasicloginapp/MainActivity.java
@@ -0,0 +1,13 @@
+package app.login.toobasicloginapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
