17e8ff72cf1af762d15ba3d24d8517853b1db3b1
Jakub <jakub.wycinka@gmail.com>
2017-04-22 10:00:34 +0200

    Jakub Wycinka

--- MyApplication/app/src/main/java/com/example/adrian/world/hello/helloworld/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/adrian/world/hello/helloworld/MainActivity.java
@@ -5,9 +5,20 @@ import android.os.Bundle;
 
 public class MainActivity extends Activity {
 
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+
+
+
+
 }
