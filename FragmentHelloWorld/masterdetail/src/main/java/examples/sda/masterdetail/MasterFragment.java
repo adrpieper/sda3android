5b1bd9b7bda1678dda5e0f7f1fa04d3d91876d28
Jakub <jakub.wycinka@gmail.com>
2017-04-22 15:50:00 +0200

    MasterDetail

---
+++ FragmentHelloWorld/masterdetail/src/main/java/examples/sda/masterdetail/MasterFragment.java
@@ -0,0 +1,36 @@
+package examples.sda.masterdetail;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * Created by RENT on 2017-04-22.
+ */
+
+public class MasterFragment extends Fragment {
+
+
+//
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+//        View view = inflater.inflate(R.layout.single_button_activity, container, false);
+//        Button button = (Button) view.findViewById(R.id.button);
+//
+//
+//        final MainActivity mainActivity = (MainActivity) getActivity();
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                mainActivity.showMessage();
+//            }
+//        });
+//
+//        return view;
+//    }
+//
+
+}
