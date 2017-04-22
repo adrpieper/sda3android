e0a430faf249bb59a62a27c0cb4d7f523ee7747b
Rafał Strzyż <rafalstrzyz@gmail.com>
2017-04-22 12:33:45 +0200

    dodanie plikow

--- LoginApp/app/src/main/java/app/login/loginapp/MainActivity.java
+++ LoginApp/app/src/main/java/app/login/loginapp/MainActivity.java
@@ -2,18 +2,22 @@ package app.login.loginapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
     private UserService service = new UserService();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final EditText nameTextField = (EditText) findViewById(R.id.nameTextField);
-        final EditText passwordTextField = (EditText) findViewById(R.id.passwordTextField);
+        EditText passwordTextField = (EditText) findViewById(R.id.passwordTextField);
         Button loginButton = (Button) findViewById(R.id.loginButton);
+
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
