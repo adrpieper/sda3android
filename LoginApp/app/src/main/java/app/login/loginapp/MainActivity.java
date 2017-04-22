0e214cc95847db5dce0d327467be0702023bbb2e
Rafał Strzyż <rafalstrzyz@gmail.com>
2017-04-22 12:28:46 +0200

    dodanie userservice

--- LoginApp/app/src/main/java/app/login/loginapp/MainActivity.java
+++ LoginApp/app/src/main/java/app/login/loginapp/MainActivity.java
@@ -1,13 +1,7 @@
 package app.login.loginapp;
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
