49bcb1a14871a81728eecd722c478671e819be09
Adrian <adrpieper@gmail.com>
2017-04-22 14:09:11 +0200

    Dodałem Fragment

---
+++ FragmentHelloWorld/app/src/main/java/examples/sda/fragmenthelloworld/ButtonFragment.java
@@ -0,0 +1,22 @@
+package examples.sda.fragmenthelloworld;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * Created by Adrian on 2017-04-22.
+ */
+
+public class ButtonFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        final Button button = new Button(getActivity());
+        button.setText("OK");
+        return button;
+    }
+}
