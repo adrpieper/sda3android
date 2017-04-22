1247cf907361d0e3d0e5f35fb71b24b82dec27fe
Paweł Falbin <p.falbin@gmail.com>
2017-04-22 11:48:58 +0200

    Merge remote-tracking branch 'origin/master' into pawel_falbin
    # Conflicts:
    #	LoginApp/app/build.gradle
    #	LoginApp/app/proguard-rules.pro
    #	LoginApp/app/src/main/res/layout/activity_main.xml
    #	LoginApp/gradle/wrapper/gradle-wrapper.properties

--- LoginApp/app/src/main/java/app/login/loginapp/MainActivity.java
+++ LoginApp/app/src/main/java/app/login/loginapp/MainActivity.java
@@ -1,13 +1,30 @@
 package app.login.loginapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
+
+    private EditText login;
+    private EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        input1 = (EditText) findViewById(R.id.editText1);
+        input2 = (EditText) findViewById(R.id.editText2);
+    }
+
+    public void wykonaj_metode(View v){
+        Intent i = new Intent(String.valueOf(getApplicationContext()));
+        i.putExtra("Parametr1", input1.getText().toString());
+        startActivity(i);
+
     }
 }
+
