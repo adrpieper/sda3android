307f47c9c7de40b97de3b54e0c5b4faa2e466610
MMia <mmiazdzyk@gmail.com>
2017-04-22 12:39:12 +0200

    Stworzylem prosty panel logowania

--- LoginApp/app/src/main/java/app/login/loginapp/MainActivity.java
+++ LoginApp/app/src/main/java/app/login/loginapp/MainActivity.java
@@ -2,12 +2,58 @@ package app.login.loginapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends Activity {
+
+    private EditText login;
+    private EditText password;
+    private Button button;
+    private Map<String, String> users = new HashMap<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createUsers();
+        login = (EditText) findViewById(R.id.editTextLogin);
+        password = (EditText) findViewById(R.id.editTextPwd);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (loginCheck(login.getText().toString(), password.getText().toString())){
+                    Toast.makeText(MainActivity.this, "zalogowano", Toast.LENGTH_SHORT).show();
+
+                }else {
+                    Toast.makeText(MainActivity.this, "bledne dane", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
+
+    private boolean loginCheck(String login, String pwd) {
+        for (Map.Entry<String, String> entry : users.entrySet()) {
+            if (entry.getKey().equals(login)) {
+                if (entry.getValue().equals(pwd)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private void createUsers() {
+        users.put("admin", "admin");
+        users.put("admin1", "admin1");
+        users.put("admin2", "admin2");
+    }
+
 }
