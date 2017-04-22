b9ec95ee516e471320c1aab6968b5f961d2fba90
MMia <mmiazdzyk@gmail.com>
2017-04-22 11:15:22 +0200

    Initial commit

---
+++ LoginApp/app/src/main/java/app/login/loginapp/MainActivity.java
@@ -0,0 +1,13 @@
+package app.login.loginapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
