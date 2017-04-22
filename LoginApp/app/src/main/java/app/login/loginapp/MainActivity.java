94374f1b78067503f72b334d800bb1a0d00d987e
KiniaCh <kinga.chruscielska@wp.pl>
2017-04-22 11:42:23 +0200

    I created a toast

--- LoginApp/app/src/main/java/app/login/loginapp/MainActivity.java
+++ LoginApp/app/src/main/java/app/login/loginapp/MainActivity.java
@@ -2,12 +2,38 @@ package app.login.loginapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends Activity {
+    @BindView(R.id.text_one)
+    protected TextView text_one;
+    @BindView(R.id.login)
+    protected EditText login;
+    @BindView(R.id.text_two)
+    protected TextView text_two;
+    @BindView(R.id.password)
+    protected EditText password;
+    @BindView(R.id.button)
+    protected Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick (R.id.button)
+    public void send (View v){
+        Toast.makeText(this, "You logged in!", Toast.LENGTH_LONG).show();
+
     }
 }
