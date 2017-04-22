72f784a02159606ca2e24a560ff175e5fa3c5b78
Adrian <adrpieper@gmail.com>
2017-04-22 11:57:36 +0200

    Dodałem przycisk logowania

--- LoginApp/app/src/main/java/app/login/loginapp/MainActivity.java
+++ LoginApp/app/src/main/java/app/login/loginapp/MainActivity.java
@@ -2,12 +2,28 @@ package app.login.loginapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText nameTextField = (EditText) findViewById(R.id.nameTextField);
+        EditText passwordTextField = (EditText) findViewById(R.id.passwordTextField);
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
