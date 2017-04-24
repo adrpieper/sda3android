fb34bf8974569d1de24d691ad4c0373399c5a2f8
macsosno <macsosno@gmail.com>
2017-04-24 19:46:21 +0200

    pliki

--- FragmentHelloWorld/masterdetail/src/main/java/examples/sda/masterdetail/DetailFragment.java
+++ FragmentHelloWorld/masterdetail/src/main/java/examples/sda/masterdetail/DetailFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**ne
  * Created by Adrian on 2017-04-22.
@@ -15,6 +16,8 @@ public class DetailFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.fragment_detail, container, false);
+        titleTextView = (TextView) view.findViewById(R.id.titleTextView);
+        contentTextView = (TextView) view.findViewById(R.id.contentTextView);
         return view;
     }
 
