ff67454e67bf6aa6e5018cae7a5782b15e5f58f6
macsosno <macsosno@gmail.com>
2017-04-24 18:10:13 +0200

    pliki

--- FragmentHelloWorld/masterdetail/src/main/java/examples/sda/masterdetail/MainActivity.java
+++ FragmentHelloWorld/masterdetail/src/main/java/examples/sda/masterdetail/MainActivity.java
@@ -1,8 +1,17 @@
 package examples.sda.masterdetail;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by maciej.sosnowski on 24.04.2017.
  */
 
-public class MainActivity {
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
