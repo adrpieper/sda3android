0d453953b32510b451f72a624130084569f6a430
MMia <mmiazdzyk@gmail.com>
2017-04-24 19:47:39 +0200

    obsluga przyciskow i wyswietalania danaych

--- FragmentHelloWorld/masterdetail/src/main/java/examples/sda/masterdetail/DetailFragment.java
+++ FragmentHelloWorld/masterdetail/src/main/java/examples/sda/masterdetail/DetailFragment.java
@@ -5,14 +5,31 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by LenovoM on 2017-04-22.
  */
 
 public class DetailFragment extends Fragment {
+    private TextView title;
+    private TextView data;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragmet_detail, container, false);
+        View view = inflater.inflate(R.layout.fragmet_detail, container, false);
+        title = (TextView) view.findViewById(R.id.title);
+        data = (TextView) view.findViewById(R.id.data);
+        return view;
+    }
+
+    public void showBasicData() {
+        title.setText("Basic data");
+        data.setText("Wyswietlanie danych podstawowych");
+    }
+
+    public void showDetailsData() {
+        title.setText("Details data");
+        data.setText("Wyswietlanie danych szczegolowych");
     }
 }
--- FragmentHelloWorld/masterdetail/src/main/java/examples/sda/masterdetail/MasterFragment.java
+++ FragmentHelloWorld/masterdetail/src/main/java/examples/sda/masterdetail/MasterFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by LenovoM on 2017-04-22.
@@ -14,6 +15,23 @@ public class MasterFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.fragment_master, container, false);
+        Button basicButton = (Button) view.findViewById(R.id.basicButton);
+        Button detailsButton = (Button) view.findViewById(R.id.detailsButton);
+        final MainActivity mainActivity = (MainActivity) getActivity();
+        basicButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainActivity.showBasicData();
+            }
+        });
+        detailsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainActivity.showDetailsData();
+            }
+        });
+
+
         return view;
     }
 }
