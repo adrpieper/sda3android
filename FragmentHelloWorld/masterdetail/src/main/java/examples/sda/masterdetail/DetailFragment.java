fb82606cc91d232e897515eb592421f581a22e29
Adrian <adrpieper@gmail.com>
2017-04-24 19:47:35 +0200

    Poprawiłem wygląd

--- FragmentHelloWorld/masterdetail/src/main/java/examples/sda/masterdetail/DetailFragment.java
+++ FragmentHelloWorld/masterdetail/src/main/java/examples/sda/masterdetail/DetailFragment.java
@@ -25,12 +25,12 @@ public class DetailFragment extends Fragment {
     }
 
     public void showBasicData() {
-        titleTextView.setText("Podstawowe dane");
+        titleTextView.setText(R.string.basic_data);
         contentTextView.setText("Adrian Pieper");
     }
 
     public void showDetailData() {
-        titleTextView.setText("Szczegóły");
+        titleTextView.setText(R.string.detail_data);
         contentTextView.setText("Trener SDA");
     }
 }
