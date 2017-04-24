00d59234f0c6576f63198a658181fa4584ec3f6b
Przemysław <przemekost@gmail.com>
2017-04-24 18:36:07 +0200

    commit

---
+++ FragmentHelloWorld/masterdetail/src/main/java/examples/sda/masterdetail/MasterFragment.java
@@ -0,0 +1,21 @@
+package examples.sda.masterdetail;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Przemysław on 2017-04-22.
+ */
+
+public class MasterFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.fragment_master, container, false);
+        return view;
+    }
+}
