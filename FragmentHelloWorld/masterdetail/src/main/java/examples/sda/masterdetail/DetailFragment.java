97da300c7919b58a4e5242264dc69dfc6e9237a7
Rafał Strzyż <rafalstrzyz@gmail.com>
2017-04-24 18:32:30 +0200

    nowa aplikacja master-detail z podziałem na ekrany

--- FragmentHelloWorld/masterdetail/src/main/java/examples/sda/masterdetail/DetailFragment.java
+++ FragmentHelloWorld/masterdetail/src/main/java/examples/sda/masterdetail/DetailFragment.java
@@ -1,10 +1,19 @@
 package examples.sda.masterdetail;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by Adrian on 2017-04-22.
  */
 
 public class DetailFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.fragment_detail, container, false);
+        return view;
+    }
 }
