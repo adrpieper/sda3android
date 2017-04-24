f405dbfd6b1340a8a90b31022e2b45e094aa6f8e
Maciej Grzesikiewicz <maciejgrz@gmail.com>
2017-04-24 19:23:34 +0200

    kolejny dzien, kolejny komit

--- FragmentHelloWorld/masterdetail/src/main/java/examples/sda/masterdetail/DetailFragment.java
+++ FragmentHelloWorld/masterdetail/src/main/java/examples/sda/masterdetail/DetailFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by Adrian on 2017-04-22.
@@ -15,6 +16,9 @@ public class DetailFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_detail, container, false);
+
+        final TextView textView=(TextView)view.findViewById(R.id.textView);
+        final TextView textView2=(TextView)view.findViewById(R.id.textView2);
         return view;
     }
 }
--- FragmentHelloWorld/masterdetail/src/main/java/examples/sda/masterdetail/MainActivity.java
+++ FragmentHelloWorld/masterdetail/src/main/java/examples/sda/masterdetail/MainActivity.java
@@ -1,13 +1,21 @@
 package examples.sda.masterdetail;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+    private DetailFragment fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragment = (DetailFragment) getFragmentManager().findFragmentById(R.id.fragment2);
+
     }
 }
