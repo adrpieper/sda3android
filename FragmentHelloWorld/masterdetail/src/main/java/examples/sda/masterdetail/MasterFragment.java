cff5be6f6afbdce2ff2903f2afd6980b1c232c88
Adrian <adrpieper@gmail.com>
2017-04-24 18:45:30 +0200

    Dodałem wczytanie widoku fragmentu z pliku

--- FragmentHelloWorld/masterdetail/src/main/java/examples/sda/masterdetail/MasterFragment.java
+++ FragmentHelloWorld/masterdetail/src/main/java/examples/sda/masterdetail/MasterFragment.java
@@ -1,10 +1,19 @@
 package examples.sda.masterdetail;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by Adrian on 2017-04-22.
  */
 
 public class MasterFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_master, container, false);
+    }
 }
