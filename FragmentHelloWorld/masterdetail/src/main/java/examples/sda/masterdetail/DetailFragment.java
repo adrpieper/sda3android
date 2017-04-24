2dff338fb0591c3502098be5ebc2f8e8f8598c53
MMia <mmiazdzyk@gmail.com>
2017-04-24 18:31:42 +0200

    powiazalem fragmenty z widokami xml

--- FragmentHelloWorld/masterdetail/src/main/java/examples/sda/masterdetail/DetailFragment.java
+++ FragmentHelloWorld/masterdetail/src/main/java/examples/sda/masterdetail/DetailFragment.java
@@ -1,10 +1,18 @@
 package examples.sda.masterdetail;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by LenovoM on 2017-04-22.
  */
 
-public class DetailFragment extends Fragment{
+public class DetailFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragmet_detail, container, false);
+    }
 }
