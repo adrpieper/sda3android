6693709f4be1e43cb76cd2a99c4ea5b21b58f9af
Adrian <adrpieper@gmail.com>
2017-04-24 19:43:27 +0200

    Dodałem wyświetlanie treści

--- FragmentHelloWorld/masterdetail/src/main/java/examples/sda/masterdetail/DetailFragment.java
+++ FragmentHelloWorld/masterdetail/src/main/java/examples/sda/masterdetail/DetailFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**ne
  * Created by Adrian on 2017-04-22.
@@ -12,15 +13,24 @@ import android.view.ViewGroup;
 
 public class DetailFragment extends Fragment {
 
+    private TextView titleTextView;
+    private TextView contentTextView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.fragment_detail, container, false);
+        titleTextView = (TextView) view.findViewById(R.id.titleTextView);
+        contentTextView = (TextView) view.findViewById(R.id.contentTextView);
         return view;
     }
 
     public void showBasicData() {
+        titleTextView.setText("Podstawowe dane");
+        contentTextView.setText("Adrian Pieper");
     }
 
     public void showDetailData() {
+        titleTextView.setText("Szczegóły");
+        contentTextView.setText("Trener SDA");
     }
 }
