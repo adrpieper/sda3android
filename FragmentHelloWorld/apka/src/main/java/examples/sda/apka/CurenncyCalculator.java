0dae6cba22522535f8f9d7af34186da9ab2a1d59
Medard <medard@mahthart.pl>
2017-04-25 19:51:48 +0200

    uzupełniam actywności

--- FragmentHelloWorld/apka/src/main/java/examples/sda/apka/CurenncyCalculator.java
+++
@@ -1,8 +0,0 @@
-package examples.sda.apka;
-
-/**
- * Created by Medard on 2017-04-25.
- */
-
-public class CurenncyCalculator {
-}
---
+++ FragmentHelloWorld/apka/src/main/java/examples/sda/apka/CurenncyCalculatorFragment.java
@@ -0,0 +1,29 @@
+package examples.sda.apka;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CurenncyCalculatorFragment extends Fragment {
+
+
+    public CurenncyCalculatorFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_curenncy_calculator, container, false);
+    }
+
+}
