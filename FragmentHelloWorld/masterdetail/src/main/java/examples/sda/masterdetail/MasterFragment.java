8b84874e6d5dfedc680aec2a504633ff4a5c1740
Maciej Grzesikiewicz <maciejgrz@gmail.com>
2017-04-25 17:58:54 +0200

    kolejny dzien, kolejny komit

--- FragmentHelloWorld/masterdetail/src/main/java/examples/sda/masterdetail/MasterFragment.java
+++ FragmentHelloWorld/masterdetail/src/main/java/examples/sda/masterdetail/MasterFragment.java
@@ -12,6 +12,7 @@ public class MasterFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
         View view = inflater.inflate(R.layout.fragment_master, container, false);
 
         Button basicDataButton = (Button) view.findViewById(R.id.basicDataButton);
