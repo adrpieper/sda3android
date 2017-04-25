13854aa97f1ddd5a7780fea9c4b388666ef4e496
Szymon Mikitiuk <sz.mktk@gmail.com>
2017-04-25 19:49:12 +0200

    Added basic functionality to methods

--- MyFragmentExample/currencycalc/src/main/java/examples/sda/currencycalc/CurrencyCalcFragment.java
+++ MyFragmentExample/currencycalc/src/main/java/examples/sda/currencycalc/CurrencyCalcFragment.java
@@ -1,10 +1,20 @@
 package examples.sda.currencycalc;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by szyms on 4/25/17.
  */
 
 public class CurrencyCalcFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_currency_calculator, container, false);
+    }
 }
