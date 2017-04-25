0142aac52408afe38b5f714c97ea2dbf56d0de04
Paweł Falbin <p.falbin@gmail.com>
2017-04-25 17:57:59 +0200

    k

--- FragmentHelloWorld/masterdetail/src/main/java/examples/sda/masterdetail/MainActivity.java
+++ FragmentHelloWorld/masterdetail/src/main/java/examples/sda/masterdetail/MainActivity.java
@@ -2,6 +2,9 @@ package examples.sda.masterdetail;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+
+import static examples.sda.masterdetail.R.id.fragment;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showBasicData(){
+        DatailFragment fragment = (DetailFragment) getFragmentManager().findFragmentById(R.id.detailFragment);
+        fragment.showBasicData();
+    }
+
+    public void showBasicData(){
+        fragment.showDetailsData();
     }
 }
--- FragmentHelloWorld/masterdetail/src/main/java/examples/sda/masterdetail/MasterFragment.java
+++ FragmentHelloWorld/masterdetail/src/main/java/examples/sda/masterdetail/MasterFragment.java
@@ -2,9 +2,5 @@ package examples.sda.masterdetail;
 
 import android.app.Fragment;
 
-/**
- * Created by Adrian on 2017-04-22.
- */
-
 public class MasterFragment extends Fragment {
 }
