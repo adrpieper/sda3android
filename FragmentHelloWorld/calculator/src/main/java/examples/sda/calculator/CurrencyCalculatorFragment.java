fa242fb8de8af245cf5eb981c08f53ec8cd6efbc
Adrian <adrpieper@gmail.com>
2017-04-25 19:45:16 +0200

    Dodałem wczytywanie layoutu do fragmentów

--- FragmentHelloWorld/calculator/src/main/java/examples/sda/calculator/CurrencyCalculatorFragment.java
+++ FragmentHelloWorld/calculator/src/main/java/examples/sda/calculator/CurrencyCalculatorFragment.java
@@ -1,10 +1,19 @@
 package examples.sda.calculator;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by Adrian on 2017-04-25.
  */
 
 public class CurrencyCalculatorFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_currency_calculator, container, false);
+    }
 }
