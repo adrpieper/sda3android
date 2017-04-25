854a5fe2854757dc7de83c2d7c38314db683629a
macsosno <macsosno@gmail.com>
2017-04-25 18:43:36 +0200

    zmiana w detail activity

--- FragmentHelloWorld/masterdetail/src/main/java/examples/sda/masterdetail/DetailActivity.java
+++ FragmentHelloWorld/masterdetail/src/main/java/examples/sda/masterdetail/DetailActivity.java
@@ -12,5 +12,7 @@ public class DetailActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+
     }
 }
