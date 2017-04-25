9442d3c778cda6250c558d706771a4b286b9a255
Medard <medard@mahthart.pl>
2017-04-25 20:31:50 +0200

    uzupełniam actywności

--- FragmentHelloWorld/apka/src/main/java/examples/sda/apka/ButtonFragment.java
+++ FragmentHelloWorld/apka/src/main/java/examples/sda/apka/ButtonFragment.java
@@ -1,11 +1,12 @@
 package examples.sda.apka;
 
 
+import android.app.Fragment;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -23,7 +24,28 @@ public class ButtonFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_button, container, false);
-    }
 
+        View view = inflater.inflate(R.layout.fragment_button, container, false);
+        Button investmentButton = (Button) view.findViewById(R.id.button1);
+        Button curenncyButton = (Button) view.findViewById(R.id.button2);
+
+
+        final MainActivity mainActivity = (MainActivity) getActivity();
+        investmentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainActivity.showInvestment();
+            }
+        });
+        curenncyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainActivity.showCurrencyCalculator();
+            }
+        });
+
+        return view;
+    }
 }
+
+
--- FragmentHelloWorld/apka/src/main/java/examples/sda/apka/MainActivity.java
+++ FragmentHelloWorld/apka/src/main/java/examples/sda/apka/MainActivity.java
@@ -1,6 +1,7 @@
 package examples.sda.apka;
 
 import android.app.Activity;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
@@ -10,5 +11,14 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
+    public void showCurrencyCalculator(){
+        final FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.main_frame, new CurenncyCalculatorFragment());
+        transaction.commit();
+    }
+    public void showInvestment(){
+        final FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.main_frame, new InvestmentFragment());
+        transaction.commit();
+    }
 }
