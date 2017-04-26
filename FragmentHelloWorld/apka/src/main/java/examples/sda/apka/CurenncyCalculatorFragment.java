958a7f4f6053e9fb7e9d57d9583947f7e2b02c40
Medard <medard@mahthart.pl>
2017-04-26 13:07:29 +0200

    kalkulator się buduje

--- FragmentHelloWorld/apka/src/main/java/examples/sda/apka/CurenncyCalculatorFragment.java
+++ FragmentHelloWorld/apka/src/main/java/examples/sda/apka/CurenncyCalculatorFragment.java
@@ -6,24 +6,59 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class CurenncyCalculatorFragment extends Fragment {
 
+    double kurs = 2.5;
+    String wynik ="";
 
     public CurenncyCalculatorFragment() {
-        // Required empty public constructor
-    }
 
+    }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_curenncy_calculator, container, false);
-    }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
+        final View view = inflater.inflate(R.layout.fragment_curenncy_calculator, container, false);
+
+        Button ok = (Button) view.findViewById(R.id.calc);
+        final EditText pln = (EditText) view.findViewById(R.id.pln);
+        final TextView wal = (TextView) view.findViewById(R.id.wal);
+        final RadioGroup gr = (RadioGroup) view.findViewById(R.id.chack);
+
+        gr.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                if(gr.getCheckedRadioButtonId() == R.id.radioButton_chf){
+                    wal.setText (R.string.chf);
+                }else if(gr.getCheckedRadioButtonId() == R.id.radioButton_euro){
+                    wal.setText (R.string.euro);
+                }else{
+                    wal.setText (R.string.usd);
+                };
+            }
+        });
+
+        ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String tmp = pln.getText().toString();
+                Calculate(tmp);
+                wal.setText(wynik);
+            }
+            private String Calculate(String input) {
+                double tmp = Double.valueOf(input);
+                double tmp1 = (double)tmp*kurs;
+                wynik = Double.toString(tmp1);
+                return wynik;
+            }
+        });
+        return view;
+    }
 }
---
+++ FragmentHelloWorld/apka/src/main/java/examples/sda/apka/waluty.java
@@ -0,0 +1,15 @@
+package examples.sda.apka;
+
+/**
+ * Created by Medard on 2017-04-26.
+ */
+
+public enum waluty {
+    USD,
+    CHF,
+    EURO;
+
+    waluty() {
+
+    }
+}
