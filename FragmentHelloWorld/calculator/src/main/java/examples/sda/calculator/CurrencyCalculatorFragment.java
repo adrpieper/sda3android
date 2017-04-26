ef52c0589582816a2c84c2d4898d8c1513ca6ccc
Maciej Grzesikiewicz <maciejgrz@gmail.com>
2017-04-26 18:29:17 +0200

    dodaje troche kalkulaThora

--- FragmentHelloWorld/calculator/src/main/java/examples/sda/calculator/CurrencyCalculatorFragment.java
+++ FragmentHelloWorld/calculator/src/main/java/examples/sda/calculator/CurrencyCalculatorFragment.java
@@ -2,16 +2,57 @@ package examples.sda.calculator;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.StringBuilderPrinter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class CurrencyCalculatorFragment extends Fragment {
 
+    private TextView plnTextView;
+    private EditText plnEditText;
+    private TextView dolarTextView;
+    private EditText dolarEditText;
+    private Button calculate;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_currency_calculator, container, false);
+
+        final View view = inflater.inflate(R.layout.fragment_currency_calculator, container, false);
+
+        plnTextView = (TextView) view.findViewById(R.id.plnTextView);
+        dolarTextView = (TextView) view.findViewById(R.id.dolarTextView);
+        plnEditText = (EditText) view.findViewById(R.id.plnEditText);
+        dolarEditText = (EditText) view.findViewById(R.id.dolarEditText);
+        calculate = (Button) view.findViewById(R.id.calculate);
+
+        calculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    String dolarString = plnEditText.getText().toString();
+                    double dolar = (Double.parseDouble(dolarString)) * 3.876;
+                    dolarTextView.setText(dolar + "");
+
+                    Toast.makeText(getActivity(), "Wpisana kwota : " + dolar, Toast.LENGTH_LONG).show();
+                } catch (NumberFormatException e) {
+                    dolarTextView.setText("0");
+                }
+
+                try {
+                    String plnString = dolarEditText.getText().toString();
+                    double pln = (Double.parseDouble(plnString)) / 3.876;
+                    plnTextView.setText(pln + "");
+                } catch (NumberFormatException e) {
+                    dolarTextView.setText("0");
+                }
+            }
+        });
+
+        return view;
     }
 }
--- FragmentHelloWorld/calculator/src/main/java/examples/sda/calculator/InvestmentFragment.java
+++ FragmentHelloWorld/calculator/src/main/java/examples/sda/calculator/InvestmentFragment.java
@@ -1,17 +1,50 @@
 package examples.sda.calculator;
 
+
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
-
+/**
+ * Created by Adrian on 2017-04-25.
+ */
 
 public class InvestmentFragment extends Fragment {
 
+    private TextView lokataTextView;
+    private TextView lataTextView;
+    private EditText lataEditText;
+    private TextView kwotaTextView;
+    private EditText kwotaEditText;
+    private TextView procentTextView;
+    private EditText procentEditText;
+    private TextView zyskTextView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_investment, container, false);
+
+        final View view = inflater.inflate(R.layout.fragment_investment, container, false);
+
+        lokataTextView = (TextView) view.findViewById(R.id.lokataTextView);
+        lataTextView = (TextView) view.findViewById(R.id.lataTextView);
+        lataEditText = (EditText) view.findViewById(R.id.lataEditText);
+        lataTextView = (TextView) view.findViewById(R.id.lataTextView);
+        kwotaTextView = (TextView) view.findViewById(R.id.kwotaTextView);
+        kwotaEditText = (EditText) view.findViewById(R.id.kwotaEditText);
+        procentTextView = (TextView) view.findViewById(R.id.procentTextView);
+        procentEditText = (EditText) view.findViewById(R.id.procentEditText);
+        zyskTextView = (TextView) view.findViewById(R.id.zyskTextView);
+
+
+
+        lataTextView.setText(lataEditText.getText());
+        kwotaTextView.setText(kwotaEditText.getText());
+        procentTextView.setText(procentEditText.getText());
+
+        return view;
     }
 }
