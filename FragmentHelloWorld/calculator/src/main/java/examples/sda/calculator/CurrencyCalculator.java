1a79c961b3d1312f65f474bfb2c237d5be22f139
Rafał Strzyż <rafalstrzyz@gmail.com>
2017-04-26 18:40:50 +0200

    dodałem pola w currencyCalculator

---
+++ FragmentHelloWorld/calculator/src/main/java/examples/sda/calculator/CurrencyCalculator.java
@@ -0,0 +1,12 @@
+package examples.sda.calculator;
+
+/**
+ * Created by RENT on 2017-04-26.
+ */
+
+public class CurrencyCalculator {
+
+    public double calc(double pln){
+        return pln*4;
+    }
+}
--- FragmentHelloWorld/calculator/src/main/java/examples/sda/calculator/CurrencyCalculatorFragment.java
+++ FragmentHelloWorld/calculator/src/main/java/examples/sda/calculator/CurrencyCalculatorFragment.java
@@ -5,15 +5,38 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
-/**
- * Created by Adrian on 2017-04-25.
- */
 
 public class CurrencyCalculatorFragment extends Fragment {
 
+    private EditText amountEditText;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_currency_calculator, container, false);
+
+        final View view = inflater.inflate(R.layout.fragment_currency_calculator, container, false);
+        amountEditText = (EditText) view.findViewById(R.id.amountEditText);
+        view.findViewById(R.id.calcButton).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                calc();
+            }
+
+            private void calc() {
+                try {
+                    String amountString = amountEditText.getText().toString();
+                    double amount = Double.parseDouble(amountString);
+                    Toast.makeText(getActivity(), "Wpisana kwota : " + amount, Toast.LENGTH_LONG).show();
+                } catch (NumberFormatException e) {
+                    Toast.makeText(getActivity(), "Proszę wpisać liczbę: ", Toast.LENGTH_LONG).show();
+                }
+            }
+
+        });
+        return view;
     }
+
 }
