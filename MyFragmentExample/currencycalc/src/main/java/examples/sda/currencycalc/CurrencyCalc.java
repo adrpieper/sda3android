091814d619c9b681c3b5186e058a0a8d61ab1434
Szymon Mikitiuk <sz.mktk@gmail.com>
2017-04-26 19:22:59 +0200

    Time's up. In the middle of CurrencyCalcFragment.java

---
+++ MyFragmentExample/currencycalc/src/main/java/examples/sda/currencycalc/CurrencyCalc.java
@@ -0,0 +1,30 @@
+package examples.sda.currencycalc;
+
+/**
+ * Created by szyms on 4/26/17.
+ */
+
+public class CurrencyCalc {
+
+    private String inputCurrency, outputCurrency;
+
+    public String getInputCurrency() {
+        return inputCurrency;
+    }
+
+    public String getOutputCurrency() {
+        return outputCurrency;
+    }
+
+    public double calc() {
+
+        /**
+         * na razie ta metoda zwraca liczbę z dupy
+         * argumentem będzie wartość
+         * sprawdzamy ifem jaka jest waluta wyjściowa (z editText/spinnera) i wyjściowa
+         * możesz zgapić kod z innego projektu realizowanego z pablojevem :)
+         */
+
+        return 42;
+    }
+}
--- MyFragmentExample/currencycalc/src/main/java/examples/sda/currencycalc/CurrencyCalcFragment.java
+++ MyFragmentExample/currencycalc/src/main/java/examples/sda/currencycalc/CurrencyCalcFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by szyms on 4/25/17.
@@ -12,16 +14,34 @@ import android.view.ViewGroup;
 
 public class CurrencyCalcFragment extends Fragment {
 
+    private EditText inputField;
+    private TextView resultField;
+    private CurrencyCalc cc;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
+
         final View view = inflater.inflate(R.layout.fragment_currency_calculator, container, false);
-        // wyświetl tekst mówiący że tutaj będzie kalkulator walut
+        final MainActivity mainActivity = (MainActivity) getActivity();
+        inputField = (EditText) view.findViewById(R.id.input_value_field);
+        resultField = (TextView) view.findViewById(R.id.result_text_view);
 
-//        view.findViewById(R.id.)
-
-
+        inputField.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calc();
+            }
+        });
         return view;
+    }
+
+    private void calc() {
+        String inputValue = inputField.getText().toString();
+        double amount = cc.calc(/*inputValue*/);
+        // teraz wyświetl tekst wraz z obliczoną wartością
+        resultField.setText("Kwota: " + amount + cc.getOutputCurrency() );
+        resultField.setVisibility(View.VISIBLE);
 
     }
 }
