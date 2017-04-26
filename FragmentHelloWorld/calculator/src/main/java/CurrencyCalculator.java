c51bc8434bed38d90e54f71990909effd44b3366
macsosno <macsosno@gmail.com>
2017-04-26 18:34:44 +0200

    -

---
+++ FragmentHelloWorld/calculator/src/main/java/CurrencyCalculator.java
@@ -0,0 +1,6 @@
+/**
+ * Created by maciej.sosnowski on 26.04.2017.
+ */
+
+public class CurrencyCalculator {
+}
--- FragmentHelloWorld/calculator/src/main/java/examples/sda/calculator/CurrencyCalculatorFragment.java
+++ FragmentHelloWorld/calculator/src/main/java/examples/sda/calculator/CurrencyCalculatorFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 /**
  * Created by Adrian on 2017-04-25.
@@ -15,5 +16,12 @@ public class CurrencyCalculatorFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_currency_calculator, container, false);
+
+        amountEditText= (EditText) view.findViewById(R.id.amountEditText);
+        view.findViewById(R.id.Button).setOnClickListener((v) -> (calc();));
+        return view;
+
+
+
     }
 }
