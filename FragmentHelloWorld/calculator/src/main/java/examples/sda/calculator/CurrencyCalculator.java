e63ddb50ca6523a769d5f006a192ed99c7c01bce
Paweł Falbin <p.falbin@gmail.com>
2017-04-26 20:22:13 +0200

    fwf2

---
+++ FragmentHelloWorld/calculator/src/main/java/examples/sda/calculator/CurrencyCalculator.java
@@ -0,0 +1,11 @@
+package examples.sda.calculator;
+
+/**
+ * Created by Notebook on 4/26/2017.
+ */
+
+public class CurrencyCalculator {
+    public double calc(double pin){
+        return pin*4;
+    }
+}
--- FragmentHelloWorld/calculator/src/main/java/examples/sda/calculator/CurrencyCalculatorFragment.java
+++ FragmentHelloWorld/calculator/src/main/java/examples/sda/calculator/CurrencyCalculatorFragment.java
@@ -5,11 +5,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class CurrencyCalculatorFragment extends Fragment {
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_currency_calculator, container, false);
+    private EditText amountEditText;
+
+
+    private void calc(){
+        String amountString = amountEditText.getText().toString();
+        try{
+            double amount = Double.parseDouble(amountString);
+            Toast.makeText(getActivity()), "Wpisana kwota po przeliczeniu : " + calculator.calc(amount);
+
+        }catch (NumberFormatException e) {
+            Toast.
+        }
     }
 }
