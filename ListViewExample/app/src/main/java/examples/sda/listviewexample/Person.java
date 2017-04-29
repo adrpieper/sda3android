4ec503113091487b99f10cdd16476566cfd9cc91
p.nowacki <p.nowacki46@gmail.com>
2017-04-29 11:58:33 +0200

    ListView

---
+++ ListViewExample/app/src/main/java/examples/sda/listviewexample/Person.java
@@ -0,0 +1,24 @@
+package examples.sda.listviewexample;
+
+/**
+ * Created by RENT on 2017-04-29.
+ */
+
+public class Person {
+
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
