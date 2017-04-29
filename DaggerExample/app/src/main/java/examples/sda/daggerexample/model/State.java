c996dd7c5577dc5d8ec9f67b90d3706a82992e61
Adrian <adrpieper@gmail.com>
2017-04-29 09:56:27 +0200

    Dodałem logike klas

--- DaggerExample/app/src/main/java/examples/sda/daggerexample/model/State.java
+++ DaggerExample/app/src/main/java/examples/sda/daggerexample/model/State.java
@@ -8,9 +8,19 @@ import javax.inject.Inject;
 
 public class State {
     private final Economy economy;
+    private int gold = 1000;
 
     @Inject
     public State(Economy economy) {
         this.economy = economy;
+    }
+
+    public void countBalance() {
+        gold += economy.countGain();
+        gold -= economy.countCost();
+    }
+
+    public int getGold() {
+        return gold;
     }
 }
