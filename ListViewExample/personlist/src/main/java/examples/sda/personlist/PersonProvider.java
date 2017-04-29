c11ba3fafe1721557af51a993f0270afd1f458ec
Maciej Grzesikiewicz <maciejgrz@gmail.com>
2017-04-29 15:25:33 +0200

    kolory bez sortowania :<

--- ListViewExample/personlist/src/main/java/examples/sda/personlist/PersonProvider.java
+++ ListViewExample/personlist/src/main/java/examples/sda/personlist/PersonProvider.java
@@ -11,9 +11,21 @@ public class PersonProvider {
 
     public List<Person> provide() {
         List<Person> persons = new ArrayList<>();
-        persons.add(new Person("Jan", 20));
-        persons.add(new Person("Halina", 25));
-        persons.add(new Person("Adam", 9));
+        persons.add(new Person("Andrzej", 20));
+        persons.add(new Person("Horacy", 25));
+        persons.add(new Person("Adaś", 9));
+        persons.add(new Person("Grażyna", 99));
+        persons.add(new Person("Irka", 55));
+        persons.add(new Person("Szafrian", 69));
+        persons.add(new Person("Elojzy", 56));
+        persons.add(new Person("Marcin", 78));
+        persons.add(new Person("Janush", 23));
+        persons.add(new Person("Grześ", 13));
+        persons.add(new Person("Stiefan", 19));
+        persons.add(new Person("Tobiasz", 45));
+
+
+
         return persons;
     }
 }
