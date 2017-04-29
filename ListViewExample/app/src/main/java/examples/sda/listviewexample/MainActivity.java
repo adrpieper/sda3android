baa1882b5009516b6594992a7e523c05daca47ed
p.nowacki <p.nowacki46@gmail.com>
2017-04-29 15:51:11 +0200

    ListView - modifikacja

--- ListViewExample/app/src/main/java/examples/sda/listviewexample/MainActivity.java
+++ ListViewExample/app/src/main/java/examples/sda/listviewexample/MainActivity.java
@@ -2,12 +2,19 @@ package examples.sda.listviewexample;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
 import android.widget.BaseAdapter;
+import android.widget.CompoundButton;
 import android.widget.ListView;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class MainActivity extends Activity {
@@ -15,24 +22,55 @@ public class MainActivity extends Activity {
     //ListView numberList;
 
     ListView personProviderList;
+    Switch sort;
 
+    PersonAdapter adapter;
+    final List<Person> personList = PersonProvider.provideList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //numberList = (ListView) findViewById(R.id.numberListView);
-        //numberList.setAdapter(new NumbersAdapter());
 
+
+        sort = (Switch) findViewById(R.id.toggleButton_sort);
         personProviderList = (ListView) findViewById(R.id.numberListView);
-        personProviderList.setAdapter(new PersonAdapter());
+        adapter = new PersonAdapter(personList, LayoutInflater.from(this)); // dodac LayoutInflater.from(this)
+
+        personProviderList.setAdapter(adapter);
+
+        sort.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+          @Override
+          public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+              if(isChecked) {
+
+                  Collections.sort(personList, new Comparator<Person>() {
+                      @Override
+                      public int compare(Person o1, Person o2) {
+                          return o1.getAge() - o2.getAge();
+                      }
+                  });
+                  adapter.notifyDataSetChanged();
+
+              } else {
+                  Collections.shuffle(personList);
+                  adapter.notifyDataSetChanged();
+              }
+          }
+      });
 
     }
 
     class PersonAdapter extends BaseAdapter {
+        private List<Person> myList;
+        private final LayoutInflater layoutInflater;
 
-        List<Person> myList = PersonProvider.provideList();
+        // dodac patrametr lay - inf do konstruktora
+        PersonAdapter(List<Person> list, LayoutInflater layoutInflater ) {
+            this.myList = list;
+            this.layoutInflater = layoutInflater;
+        }
 
         @Override
         public int getCount() {
@@ -53,15 +91,17 @@ public class MainActivity extends Activity {
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            TextView textView;
+
+
+            View personView;
             if(convertView == null) {
-                textView = new TextView(MainActivity.this);
+                personView = layoutInflater.inflate(R.layout.activity_main,parent,false);
             } else {
-                textView = (TextView) convertView;
+                personView =  convertView;
             }
 
-            textView.setText(getItem(position).toString());
-            return textView;
+            //(TextView) textView.setText(getItem(position).toString());
+            return personView;
         }
     }
 }
