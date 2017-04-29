02348f071c4109efffd1578ba5493287797e2993
Adrian <adrpieper@gmail.com>
2017-04-29 11:54:37 +0200

    Skończyłem przykład

--- ListViewExample/app/src/main/java/examples/sda/listviewexample/MainActivity.java
+++ ListViewExample/app/src/main/java/examples/sda/listviewexample/MainActivity.java
@@ -2,6 +2,12 @@ package examples.sda.listviewexample;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +15,34 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView numbersListView = (ListView) findViewById(R.id.numbersListView);
+        numbersListView.setAdapter(new NumbersAdapter());
+    }
+
+    class NumbersAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return 'z'-'a' + 1;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return (char) (position+'a');
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            TextView textView = new TextView(MainActivity.this);
+            textView.setText(getItem(position).toString());
+            textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 32);
+            return textView;
+        }
     }
 }
