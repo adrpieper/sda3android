46655c933a975a538676006898334e741dd93397
Przemysław <przemekost@gmail.com>
2017-04-29 15:05:35 +0200

    sort Switch Added

--- ListViewExample/listviewperson/src/main/java/examples/sda/listviewperson/MainActivity.java
+++ ListViewExample/listviewperson/src/main/java/examples/sda/listviewperson/MainActivity.java
@@ -1,42 +1,83 @@
 package examples.sda.listviewperson;
 
 import android.app.Activity;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.CompoundButton;
 import android.widget.ListView;
+import android.widget.Switch;
 import android.widget.TextView;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public class MainActivity extends Activity {
 
     PersonProvider personProvider;
-
-
-    Resources resources = getResources();
+    private PeopleListAdapter adapter;
+    private List<Person> personList;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        personProvider =new PersonProvider(getResources());
-        ListView peopleList = (ListView) findViewById(R.id.peopleList);
-        peopleList.setAdapter(new PeopleListAdapter());
+
+
+
+        personProvider = new PersonProvider(getResources());
+
+        ListView peopleListView = (ListView) findViewById(R.id.peopleList);
+        personList = personProvider.createPerson();
+        adapter = new PeopleListAdapter(personList);
+        peopleListView.setAdapter(adapter);
+
+        Switch sortSwitch = (Switch) findViewById(R.id.sortSwitch);
+
+        sortSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                if(isChecked == true) {
+                    Collections.sort(personList, new Comparator<Person>() {
+                        @Override
+                        public int compare(Person o1, Person o2) {
+                            return o1.getAge() - o2.getAge();
+                        }
+                    });
+                }else{
+                    Collections.sort(personList, new Comparator<Person>() {
+                        @Override
+                        public int compare(Person o1, Person o2) {
+                            return o2.getAge() - o1.getAge();
+                        }
+                    });
+                }
+                adapter.notifyDataSetChanged();
+            }
+        });
 
     }
 
     class PeopleListAdapter extends BaseAdapter {
 
+        private List<Person> personList;
+
+        public PeopleListAdapter(List<Person> personList) {
+            this.personList = personList;
+        }
+
         @Override
         public int getCount() {
-            return personProvider.personList.size();
+            return personList.size();
         }
 
         @Override
         public Object getItem(int position) {
-            return position + 1;
+            return personList.get(position).getName() + " " + personList.get(position).getSurname() + " " + personList.get(position).getAge();
         }
 
         @Override
@@ -47,10 +88,9 @@ public class MainActivity extends Activity {
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
             TextView textView = new TextView(MainActivity.this);
-            for (int i = 0; i < personProvider.personList.size(); i++) {
-                textView.setText((personProvider.personList.get(i).getName() + " " +
-                        personProvider.personList.get(i).getSurname() + " " + personProvider.personList.get(i).getAge()).toString());
-            }
+
+            textView.setText(getItem(position).toString());
+
             return textView;
         }
     }
--- ListViewExample/listviewperson/src/main/java/examples/sda/listviewperson/PersonProvider.java
+++ ListViewExample/listviewperson/src/main/java/examples/sda/listviewperson/PersonProvider.java
@@ -12,16 +12,17 @@ import java.util.List;
 public class PersonProvider {
 
 
+
     private Resources resources;
+    private List<Person> personList;
 
     public PersonProvider(Resources resources) {
         this.resources = resources;
     }
 
-    List<Person> personList = new ArrayList<>();
-
     public List<Person> createPerson() {
 
+        personList = new ArrayList<>();
         for (int i = 0; i < resources.getStringArray(R.array.names).length; i++) {
             Person person = new Person(resources.getStringArray(R.array.names)[i],
                     resources.getStringArray(R.array.surnames)[i], resources.getIntArray(R.array.ages)[i]);
