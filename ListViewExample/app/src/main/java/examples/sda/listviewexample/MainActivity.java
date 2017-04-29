dcebca8903f1ffe890c1ab6a8d04cc7f5e4dd510
Maciej Grzesikiewicz <maciejgrz@gmail.com>
2017-04-29 11:54:20 +0200

    ni wiem co to

--- ListViewExample/app/src/main/java/examples/sda/listviewexample/MainActivity.java
+++ ListViewExample/app/src/main/java/examples/sda/listviewexample/MainActivity.java
@@ -2,6 +2,13 @@ package examples.sda.listviewexample;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +16,36 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView numbeListView = (ListView) findViewById(R.id.numberListView);
+        numbeListView.setAdapter(new NumbersAdapter());
+
+    }
+
+    class  NumbersAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return 'x'-'a' +1;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return (char) (position+'a');
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            TextView textView = new TextView(MainActivity.this);
+            textView.setText(getItem(position).toString());
+
+
+            return textView;
+        }
     }
 }
