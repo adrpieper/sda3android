178f2f8d9857da78edafee8e0a750047e450151a
Przemysław <przemekost@gmail.com>
2017-04-29 09:19:07 +0200

    init

---
+++ DaggerExample/app/src/main/java/examples/sda/daggerexample/MainActivity.java
@@ -0,0 +1,13 @@
+package examples.sda.daggerexample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
