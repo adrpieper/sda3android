1a28445ae64f06b23747727479e8f8bf4ce7edce
Maciej Grzesikiewicz <maciejgrz@gmail.com>
2017-04-29 10:24:12 +0200

    jakies stare

--- DaggerExample/app/src/main/java/examples/sda/daggerexample/StateProvider.java
+++ DaggerExample/app/src/main/java/examples/sda/daggerexample/StateProvider.java
@@ -6,20 +6,15 @@ import examples.sda.daggerexample.model.Economy;
 import examples.sda.daggerexample.model.Population;
 import examples.sda.daggerexample.model.State;
 
-/**
- * Created by RENT on 2017-04-29.
- */
 
 public class StateProvider {
 
-    public static State provide(){
+    public static State provide() {
 
         Population population = new Population();
         Army army = new Army(population);
         Building building = new Building();
-        Economy economy = new Economy(army,building,population);
-
-
-
+        Economy economy = new Economy(army, building, population);
+        return new State(economy);
     }
 }
