6d7c143a00c8d6405a15d364d52959556e09448a
Paweł Falbin <p.falbin@gmail.com>
2017-05-04 18:00:14 +0200

    nowe4

---
+++ FragmentHelloWorld/MyApplication2/app/src/main/java/app/login/myapplication/User.java
@@ -0,0 +1,20 @@
+package app.login.myapplication;
+
+
+public class User {
+    private String name;
+    private int age;
+
+    public void setAge(int Age){
+        this.age = age;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public int getAge(){
+        return age;
+    }
+    public String getName(){
+        return name;
+    }
+}
