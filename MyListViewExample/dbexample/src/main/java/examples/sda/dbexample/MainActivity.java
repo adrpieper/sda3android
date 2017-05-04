5d9f4c1ff2059373768632932c523d99aee634c2
Szymon Mikitiuk <sz.mktk@gmail.com>
2017-05-04 18:46:47 +0200

    Data binding in XML

--- MyListViewExample/dbexample/src/main/java/examples/sda/dbexample/MainActivity.java
+++ MyListViewExample/dbexample/src/main/java/examples/sda/dbexample/MainActivity.java
@@ -1,30 +1,41 @@
 package examples.sda.dbexample;
 
 import android.app.Activity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.widget.TextView;
+
+import examples.sda.dbexample.databinding.ActivityMainBinding;
 
 public class MainActivity extends Activity {
 
-    TextView nameTextView, ageTextView;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setUser(new User("Jan", 28));
+        // no longer necessary!
+        //setContentView(R.layout.activity_main);
 
-        nameTextView = (TextView) findViewById(R.id.name_text_view);
-        ageTextView = (TextView) findViewById(R.id.age_text_view);
+/*        nameTextView = (TextView) findViewById(R.id.name_text_view);
+        ageTextView = (TextView) findViewById(R.id.age_text_view);*/
+
+/*      another way of writing that:
+        nameTextView = binding.nameTextView;
+        ageTextView = binding.ageTextView;*/
 
 /*        User user = new User("Jan", 28);
         User user2 = new User("Kasia", 24);*/
 
-        bindUser(new User("Jan", 28));
+//        bindUser(new User("Jan", 28));
+
+
     }
 
-    private void bindUser(User user) {
+/*    private void bindUser(User user) {
 
-        nameTextView.setText(user.getName());
-        ageTextView.setText(String.valueOf(user.getAge()));
-    }
+        binding.nameTextView.setText(user.getName());
+        binding.ageTextView.setText(String.valueOf(user.getAge()));
+    }*/
 }
