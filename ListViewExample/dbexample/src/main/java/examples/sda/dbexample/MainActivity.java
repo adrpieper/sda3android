5e0eb3985b54e340fb975f7571bba95bf01f4020
AngelikaWittbrodt <angwittb@gmail.com>
2017-05-04 18:35:07 +0200

    Done binding

--- ListViewExample/dbexample/src/main/java/examples/sda/dbexample/MainActivity.java
+++ ListViewExample/dbexample/src/main/java/examples/sda/dbexample/MainActivity.java
@@ -1,13 +1,29 @@
 package examples.sda.dbexample;
 
 import android.app.Activity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import examples.sda.dbexample.databinding.ActivityMainBinding;
 
 public class MainActivity extends Activity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        bindUser(new User("Jan", 20));
+    }
+
+    public void bindUser(User user) {
+
+        binding.nameEditText.setText(user.getName());
+        binding.ageEditText.setText(String.valueOf(user.getAge()));
+
     }
 }
