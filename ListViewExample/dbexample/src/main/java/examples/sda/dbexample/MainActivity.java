c96d314a5db7a647a5a1957f44f77f1e29af55f6
Maciej Grzesikiewicz <maciejgrz@gmail.com>
2017-05-04 19:27:55 +0200

    next

--- ListViewExample/dbexample/src/main/java/examples/sda/dbexample/MainActivity.java
+++ ListViewExample/dbexample/src/main/java/examples/sda/dbexample/MainActivity.java
@@ -1,13 +1,29 @@
 package examples.sda.dbexample;
 
 import android.app.Activity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import examples.sda.dbexample.databinding.ActivityMainBinding;
 
 public class MainActivity extends Activity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        final User user = new User("Jan", 20);
+        binding.setUser(user);
+        binding.nextUserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                user.setName("Adam");
+                binding.invalidateAll();
+            }
+        });
     }
 }
