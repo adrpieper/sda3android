0f259f17aa49dc99f1a3f29ca52b0c52ae1b3bca
Adrian <adrpieper@gmail.com>
2017-05-04 18:11:32 +0200

    Wyświeliłem dane Usera

--- ListViewExample/dbexample/src/main/java/examples/sda/dbexample/MainActivity.java
+++ ListViewExample/dbexample/src/main/java/examples/sda/dbexample/MainActivity.java
@@ -2,12 +2,27 @@ package examples.sda.dbexample;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+    private TextView nameTextView;
+    private TextView ageTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nameTextView = (TextView) findViewById(R.id.nameTextView);
+        ageTextView = (TextView) findViewById(R.id.ageTextView);
+
+        bindUser(new User("Jan", 20));
+    }
+
+    private void bindUser(User user) {
+        nameTextView.setText(user.getName());
+        ageTextView.setText(String.valueOf(user.getAge()));
     }
 }
--- ListViewExample/dbexample/src/main/java/examples/sda/dbexample/User.java
+++ ListViewExample/dbexample/src/main/java/examples/sda/dbexample/User.java
@@ -9,6 +9,11 @@ public class User {
     private String name;
     private int age;
 
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
     public void setAge(int age) {
         this.age = age;
     }
