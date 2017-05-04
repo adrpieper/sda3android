f6161c428aeecc7f09eec13a6ffef306ae58a61e
Szymon Mikitiuk <sz.mktk@gmail.com>
2017-05-04 18:23:12 +0200

    Used data binding for the first time on dbexample module

--- MyListViewExample/dbexample/src/main/java/examples/sda/dbexample/MainActivity.java
+++ MyListViewExample/dbexample/src/main/java/examples/sda/dbexample/MainActivity.java
@@ -2,12 +2,29 @@ package examples.sda.dbexample;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+    TextView nameTextView, ageTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nameTextView = (TextView) findViewById(R.id.name_text_view);
+        ageTextView = (TextView) findViewById(R.id.age_text_view);
+
+/*        User user = new User("Jan", 28);
+        User user2 = new User("Kasia", 24);*/
+
+        bindUser(new User("Jan", 28));
+    }
+
+    private void bindUser(User user) {
+
+        nameTextView.setText(user.getName());
+        ageTextView.setText(String.valueOf(user.getAge()));
     }
 }
---
+++ MyListViewExample/dbexample/src/main/java/examples/sda/dbexample/User.java
@@ -0,0 +1,34 @@
+package examples.sda.dbexample;
+
+/**
+ * Created by szyms on 5/4/17.
+ */
+
+public class User {
+
+    private String name;
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+
+}
