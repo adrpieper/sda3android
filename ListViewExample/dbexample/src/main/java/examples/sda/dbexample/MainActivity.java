1c6fabf3d42b6be0b23742d2e70b3f32932beb8f
AngelikaWittbrodt <angwittb@gmail.com>
2017-05-04 19:31:01 +0200

    Before Observable

--- ListViewExample/dbexample/src/main/java/examples/sda/dbexample/MainActivity.java
+++ ListViewExample/dbexample/src/main/java/examples/sda/dbexample/MainActivity.java
@@ -3,6 +3,8 @@ package examples.sda.dbexample;
 import android.app.Activity;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import examples.sda.dbexample.databinding.ActivityMainBinding;
@@ -15,15 +17,28 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final User user = new User("Anna", 35);
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        bindUser(new User("Jan", 20));
+        binding.setUser(new User("Jan Kowalski", 20));
+        binding.setUser(user);
+
+        //nie mamy obserwatora
+
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.setUser(new User("Tomasz Nowicki", 50));
+                user.setName("Ola");
+            }
+        });
+        //bindUser(new User("Jan", 20));
     }
 
-    public void bindUser(User user) {
+    /*public void bindUser(User user) {
 
         binding.nameEditText.setText(user.getName());
         binding.ageEditText.setText(String.valueOf(user.getAge()));
 
-    }
+    }*/
 }
--- ListViewExample/dbexample/src/main/java/examples/sda/dbexample/User.java
+++ ListViewExample/dbexample/src/main/java/examples/sda/dbexample/User.java
@@ -1,10 +1,15 @@
 package examples.sda.dbexample;
 
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
+import java.util.Observable;
+
 /**
  * Created by angelika on 04.05.17.
  */
 
-public class User {
+public class User extends BaseObservable {
 
     private String name;
     private int age;
@@ -18,16 +23,20 @@ public class User {
         return name;
     }
 
+    @Bindable
     public int getAge() {
         return age;
     }
 
     public void setName(String name) {
         this.name = name;
+        notifyPropertyChanged(BR.user);
     }
 
+    @Bindable
     public void setAge(int age) {
         this.age = age;
+        notifyPropertyChanged(BR.age);
     }
 
 
