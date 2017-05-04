47702432fb7386735836395a46bde06d1bcddcf8
Szymon Mikitiuk <sz.mktk@gmail.com>
2017-05-04 19:47:55 +0200

    Made User class observable; applying Data Binding to personlist module

--- MyListViewExample/dbexample/src/main/java/examples/sda/dbexample/User.java
+++ MyListViewExample/dbexample/src/main/java/examples/sda/dbexample/User.java
@@ -1,6 +1,7 @@
 package examples.sda.dbexample;
 
 import android.databinding.BaseObservable;
+import android.databinding.Bindable;
 
 /**
  * Created by szyms on 5/4/17.
@@ -18,16 +19,20 @@ public class User extends BaseObservable {
 
     public void setName(String name) {
         this.name = name;
+        notifyPropertyChanged(BR.name);
     }
 
     public void setAge(int age) {
         this.age = age;
+        notifyPropertyChanged(BR.age);
     }
 
+    @Bindable
     public String getName() {
         return name;
     }
 
+    @Bindable
     public int getAge() {
         return age;
     }
